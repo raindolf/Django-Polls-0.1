97d77d52326fa95fe9f9cda7afccf3149b9ea025
Raindolf owusu <iraindolf@yahoo.com>
2013-07-25 18:31:40 -0400

    wrote test for the view of past poll

--- polls/tests.py
+++ polls/tests.py
@@ -1,9 +1,4 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
 
-Replace this with more appropriate tests for your application.
-"""
 import datetime
 
 from django.utils import timezone
